9be76afc677e5d9235e41d32cda34f496c7dc66d
boulbeba115 <55366002+boulbeba115@users.noreply.github.com>
2022-01-24 14:19:34 +0100

    Update configuration.dart

--- lib/configs/configuration.dart
+++ lib/configs/configuration.dart
@@ -2,8 +2,8 @@
 
 const API_URL = 'shopapp-cd9dd-default-rtdb.europe-west1.firebasedatabase.app';
 const AUTH_URL =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCWaPvbGKxxnmpN94OaCDhDo2T1CcRu72A';
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key={}';
 const SIGNIN_URL =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCWaPvbGKxxnmpN94OaCDhDo2T1CcRu72A';
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={}';
 const SINGUP_URL =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCWaPvbGKxxnmpN94OaCDhDo2T1CcRu72A';
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={}';
